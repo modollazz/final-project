0753ad3e9cae2ebf6babe5004b9cdb25bed376ea
morgan <morganwebb1008@gmail.com>
2026-03-09 09:48:45 -0400

    first

---
+++ quiz.py
@@ -0,0 +1,13 @@
+
+if __name__ == '__main__':
+    print('good luck on the quiz!')
+
+    questions = [
+        'What pop singer shaved their hair off due to a mental breakdown in 2007?',
+        ''
+    ]
+
+    answers = [
+        'britney spears ',
+        'falcon'
+    ]
