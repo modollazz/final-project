01c6563d48ab073ca69e81f18482d5005c19f2fc
morgan <morganwebb1008@gmail.com>
2026-03-12 11:52:05 -0400

    done

--- quiz.py
+++ quiz.py
@@ -4,10 +4,21 @@ if __name__ == '__main__':
 
     questions = [
         'What pop singer shaved their hair off due to a mental breakdown in 2007?',
+        'What song made Justin Bieber famous?'
+        'How many members are in Destiny\'s Child?'
+        'In what year, did the King of Pop Michael Jackson die in?'
+        'Which female singer owns the makeup brand Fenty Beauty?'
+        'What is the name of the artist that showed up to the MTV Awards in a meat dress?'
+        'Which Harlem rapper do Rihanna have three kids with?'
         ''
-    ]
+    ] 
 
     answers = [
         'britney spears ',
-        'falcon'
-    ]
+        'baby'
+        '4'
+        '2009'
+        'rihanna'
+        'lady gaga'
+        'asap rocky'
+    ]  
